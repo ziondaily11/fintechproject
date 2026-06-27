8067bff3ad3cd42c6f7a3ef5034362931038f529
TheeAnalyst_ke <ziondaily11@gmail.com>
2026-06-27 20:10:52 +0300

    t

--- Safaricom Data Project.py
+++ Safaricom Data Project.py
@@ -245,7 +245,7 @@ def show_home():
         st.write(
           "🔍 Biggest finding: Fraudulent transactions are on average 72% larger than legitimate ones (KES 2,535 vs KES 1,476). And for transactions above KES 5,000, the fraud rate shoots to 9.65% — nearly 1 in 10. Kenyans sending big money are the primary target."  
         )
-        st.markdown("-")
+    st.markdown("-")
     #GRAPHS
     #fraude rate per amount
     
