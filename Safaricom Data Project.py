168aae1e670d1e04eeba11913ac18d07b51d0ba5
TheeAnalyst_ke <ziondaily11@gmail.com>
2026-06-30 04:59:22 +0300

    b

--- Safaricom Data Project.py
+++ Safaricom Data Project.py
@@ -320,7 +320,7 @@ def show_home():
     ))
     transaction_split_pie.update_traces(
         outsidetextfont= dict(size= 12),
-        domain= dict(x=[0.1, 0.9], y=[0.1, 0.9])
+        domain= dict(x=[0.1, 0.7], y=[0.1, 0.7])
         )
     transaction_split_pie.update_layout(
         margin= dict(t= 40, b= 10, l= 10, r= 10)
