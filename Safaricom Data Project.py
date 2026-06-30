5443487df74562c3b4cc4805720fb1e410747af0
TheeAnalyst_ke <ziondaily11@gmail.com>
2026-06-30 06:23:53 +0300

    x

--- Safaricom Data Project.py
+++ Safaricom Data Project.py
@@ -347,7 +347,8 @@ def show_home():
         x= tran_per_hour.index,
         y= tran_per_hour.values,
         title= "Transaction by Hour of the Day",
-        markers= True
+        markers= True,
+        color_discrete_sequence= ["#90F269"]
     )
     hourly_tran_bar.update_traces(
         line= dict(
